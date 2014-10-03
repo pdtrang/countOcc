9e26e56d4c6f6fe9da135820bd71055eed94631d
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2014-10-03 10:43:07 -0500

    Create readfile.go

---
+++ readfile.go
@@ -0,0 +1,37 @@
+package readfile
+
+import (
+     "os"
+    "fmt"
+    "bufio"
+    "bytes"
+    //"log"
+)
+
+func ReadFASTA(sequence_file string) []byte {
+    f, err := os.Open(sequence_file)
+    if err != nil {
+        fmt.Printf("%v\n", err)
+        os.Exit(1)
+    }
+    defer f.Close()
+    br := bufio.NewReader(f)
+    byte_array := bytes.Buffer{}
+    var isPrefix bool
+    _, isPrefix, err = br.ReadLine()
+    if err != nil || isPrefix {
+        fmt.Printf("%v\n", err)
+        os.Exit(1)
+    }
+    //fmt.Printf("%s",line)
+    var line []byte
+    for {
+        line, isPrefix, err = br.ReadLine()
+        if err != nil || isPrefix {
+        break
+        } else {
+            byte_array.Write(line)
+        }
+    }
+    return []byte(byte_array.String())
+}
